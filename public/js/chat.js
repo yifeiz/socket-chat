6d879489b9e5fb1e43dd14adc2ab754047076b60
Yifei Zhang <yifei7.zhang@gmail.com>
2020-09-10 15:43:26 -0400

    location sharing

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,5 @@
 const socket = io();
 
-// // Event names must match to that on server
-// socket.on("countUpdated", count => {
-//   console.log(`The count has been updated! ${count}`);
-// });
-
-// document.querySelector("#increment").addEventListener("click", () => {
-//   console.log("click");
-//   socket.emit("increment");
-// });
-
 socket.on("welcomeEvent", message => {
   console.log(message);
 });
@@ -21,5 +11,23 @@ socket.on("message", msg => {
 document.querySelector("#message-form").addEventListener("submit", e => {
   e.preventDefault();
   const message = e.target.elements.message.value;
-  socket.emit("messageSent", message);
+  socket.emit("sendMessage", message, error => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(`Message delivered`);
+  });
+});
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser :(");
+  }
+
+  navigator.geolocation.getCurrentPosition(position => {
+    const { latitude, longitude } = position.coords;
+    socket.emit("sendLocation", { latitude, longitude }, () => {
+      console.log("Location shared!");
+    });
+  });
 });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app);
@@ -17,10 +18,26 @@ app.use(express.static(publicDirectoryPath));
 
 io.on("connection", socket => {
   console.log("New WebSocket Connection");
-
   socket.emit("welcomeEvent", "Welcome!");
-  socket.on("messageSent", message => {
+  socket.broadcast.emit("message", "A new user has joined!");
+
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed!");
+    }
+
     io.emit("message", message);
+    callback("Delivered");
+  });
+
+  socket.on("sendLocation", ({ latitude, longitude }, callback) => {
+    io.emit("message", `https://google.com/maps?q=${latitude},${longitude}`);
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
   });
 });
 
